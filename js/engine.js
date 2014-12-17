082d15f5aa4055d2a76c2b20d182e6616e9925a9
Alex Liu <yuningalexliu@gmail.com>
2014-12-17 00:34:25 -0500

    implement countdown and also refactor some methods

--- js/engine.js
+++ js/engine.js
@@ -1,38 +1,95 @@
 
+// initiate variables
 var answer = Math.floor((Math.random() * 100) + 1);
 var $feedback = $("#feedback");
-var guessLimit = 10;
+var guessLimit = 5;
+var remainingGuesses = 5;
+var previousGuessesArray = [];
 var currentSubmission;
-var previousGuess;
 
-function compare(submission, answer) {
-	if (submission < answer) {
-		$feedback.text("Your guess is too low!");
-	} else if (submission > answer) {
-		$feedback.text("Your guess is too high!");
+// perform upon html page load
+
+$("#guesses").text(remainingGuesses);
+$("#feedback").text("Ready to start when you are...");
+
+// functions below
+
+function generateRandomAnswer() {
+	answer = Math.floor((Math.random() * 100) + 1);
+}
+
+function doesGameContinue() {
+  if (remainingGuesses < 1) {
+  	$("#guesses").text(remainingGuesses);
+  	return false;
+  } else {
+  	return true;
+  }
+}
+
+function isSubmissionCorrect(submission, answer) {
+	if (submission < answer || submission > answer) {
+		return false;
 	} else {
-		$feedback.text("YOU GOT IT!!!");
+		return true;
 	}
 }
 
-// Event-driven code below
+function giveFeedback(submission, answer) {
+	if (submission < answer) {
+		$feedback.text("Please try a higher number.");
+	} else {
+		$feedback.text("Please try a lower number.");
+	}
+}
+
+function decreaseRemainingGuesses() {
+	remainingGuesses -= 1;
+	$("#guesses").text(remainingGuesses);
+}
+
+function endGame(message) {
+	$("#submit").hide();
+	$("#hint").hide();
+	$feedback.text(message);
+}
+
+function reset(message) {
+	$("#input").val("");
+	$feedback.text(message);
+	remainingGuesses = guessLimit;
+	$("#guesses").text(remainingGuesses);
+	$("#submit").show();
+	$("#hint").show();
+	answer = Math.floor((Math.random() * 100) + 1);
+}
+
+// event-driven code below
 
 $("#submit").on("click", function() {
 	currentSubmission = $("#input").val();
+	previousGuessesArray.push(currentSubmission);
 	if (currentSubmission > 0 && currentSubmission <= 100) {
-		compare(currentSubmission, answer);
+		decreaseRemainingGuesses();
+		if (doesGameContinue() === true && isSubmissionCorrect(currentSubmission, answer) === true) {
+			endGame("Nice job guessing " + answer + "! Play again?");
+		} else if (doesGameContinue() === true && isSubmissionCorrect(currentSubmission, answer) === false) {
+			giveFeedback(currentSubmission, answer);
+		} else if (doesGameContinue() === false && isSubmissionCorrect(currentSubmission, answer) === true) {
+			endGame("You got " + answer + " on the last go! Play again?");
+		} else {
+			endGame("Sorry, you lost the game. The correct answer was " + answer + ". Play again?");
+		}
 	} else {
 		$("#input").val("");
 		$feedback.text("Please try again - your input must be between 1 and 100.");
 	}
-})
+});
 
 $("#play-again").on("click", function() {
-	$("#input").val("");
-	$feedback.text("Waiting for you to press Submit...");
-})
+	reset("Ready to start when you are...");
+});
 
 $("#hint").on("click", function() {
-	$("#input").val("");
-	$feedback.text("The answer is " + answer + ". Hope you play again!");
-})
+	endGame("The answer is " + answer + ". Hope you play again!");
+});
